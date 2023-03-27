c062fb5f1237def472e5652becf4f9a17e15e6c6
Rawan-Hamza <rawanhamza@hotmail.com>
2023-03-27 10:28:54 +0100

    fixes build app

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const configuration = new Configuration({
   organization: "org-UNC80YuTHlVMW0RHf4FRpumZ",
